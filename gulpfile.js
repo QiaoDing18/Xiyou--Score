9dce3e6666e998a0d4526d3055ec2d9353d0ff61
Your Name <you@example.com>
2017-05-09 08:13:52 +0800

    2017-5-9

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp--ruby-sass');
+var concat = require('gulp-concat');
+var webpack = require('gulp-webpack');
+var imagemin = require('gulp-imagemin');
+var webpackConfig = require('./webpack.config.js');
+
+
+gulp.task('task')
