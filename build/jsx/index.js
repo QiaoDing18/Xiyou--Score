99f0a847b53b091c12fc3951cc961a80c363b973
QiaoDing18 <tjoe18@126.com>
2017-08-22 02:22:34 +0800

    Update index.js

--- build/jsx/index.js
+++ build/jsx/index.js
@@ -1,41 +1,94 @@
 (function(){
-  var iStruct = function(){};
-  iStruct.prototype = {
-    constructor: iStruct,
-    iAjax: function(url, data, type, callback){
+  var Base = function(){};
+  Base.prototype = {
+    constructor: Base,
+    baseAjax: function(url, data, type, callback){
       $.ajax({
         url: url,
         type: type,
         data: data,
-        dataTye: 'jsonp',
+        dataType: 'jsonp',
         success: function(data){
           callback(data);
         }
       });
     }
   };
-  var struct = {
-    struct: function(){
-      return new iStruct();  
+  var base = {
+    base: function(){
+      return new Base();
     }
   };
 
   var InputMainBox = React.createClass({
+    mixins: [base],
+
+    data: {
+      username: '',
+      password: '',
+      session: '',
+      verCode: '',
+    },
+
+    getInitialState: function(){
+      return {
+        verCode: null,
+      };
+    },
+
+    componentWillMount: function(){
+      that = this;
+      this.base().baseAjax('http://scoreapi.xiyoumobile.com/users/verCode', null, 'GET', function(data){
+        that.data.session = data.result.session;
+        that.setState({ verCode: data.result.verCode });
+      });
+    },
+
+    checkChangeVerCode: function(){
+      that = this;
+      this.base().baseAjax('http://scoreapi.xiyoumobile.com/users/verCode', null, 'GET', function(data){
+        that.data.session = data.result.session;
+        that.setState({ verCode: data.result.verCode });
+      });
+    },
+
+    loginHandle: function(){
+      var that = this;
+      this.data.username = ReactDOM.findDOMNode(this.refs.num).value;
+      this.data.password = ReactDOM.findDOMNode(this.refs.password).value;
+      this.data.verCode = ReactDOM.findDOMNode(this.refs.vercode).value;
+      ReactDOM.findDOMNode(this.refs.vercode).value = null;
+      this.base().baseAjax('http://scoreapi.xiyoumobile.com/users/login', that.data, 'GET', function(data){
+        if(data.error == false){
+          $.cookie('username', that.data.username);
+          $.cookie('password', that.data.password);
+          $.cookie('session', data.result.session);
+          $.cookie('verCode', that.data.verCode);
+          window.location.href = 'score.html';
+        }else{
+          alert("请检输入");
+          that.checkChangeVerCode();
+        }
+      });
+    },
+
     render: function(){
       return (
-        <div id="login" ref="login">
+        <form id="login" ref="login">
           <div id="title"></div>
           <div id="inputBox">
             <h5>学 号 </h5>
-            <input type="text" id="num" placeholder="学号"/>
+            <input type="text" id="num" ref="num" placeholder="学号"/>
             <h5>密 码 </h5>
-            <input type="passWord" id="password" placeholder="密码"/>
+            <input type="passWord" id="password" ref="password" placeholder="密码"/>
             <h5>验 证 码 </h5>
-            <input type="text" id="vercode" placeholder="验证码"/>
-            <img src="" />
+            <input type="text" id="vercode" ref="vercode" placeholder="验证码"/>
+            <img src={this.state.verCode} onClick={this.checkChangeVerCode}/>
           </div>
-          <button><p>登录</p></button>
-        </div>
+          <div id="button" onClick={this.loginHandle}>
+            <p>登录</p>
+          </div>
+        </form>
       );
     }
   });
