89a9b3168714b4f75a7a7e8703d9108b601cdc05
QiaoDing18 <tjoe18@126.com>
2017-09-14 22:22:55 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,34 @@ var gulp = require('gulp');
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var webpack = require('gulp-webpack');
+var runSequence = require('run-sequence');
 var webpackConfig = require('./webpack.config.js');
 
-//样式
-gulp.task('styles', function(){
-  return sass('src/sass/*.scss')
-    .pipe(minifycss())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('src/css_min'));
-});
-
-//脚本
-gulp.task('scripts', function(){
+gulp.task('webpack', function(){
+  var myConfig = Object.create(webpackConfig);
   return  gulp.src('build/jsx/*.js')
-  .pipe(webpack(webpackConfig))
+  .pipe(webpack(myConfig))
   .pipe(uglify())
   .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest('build/jsx_build'));
 });
 
+//样式
+gulp.task('styles', function(){
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('src/css_min'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('build/jsx/*', ['webpack']);
+  gulp.watch('src/sass/*', ['styles']);
+});
+
 gulp.task('default', function(){
-  gulp.start('styles', 'scripts');
+  runSequence('webpack', 'styles', 'watch');
 });
